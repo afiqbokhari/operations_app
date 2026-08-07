cc83a4dfa30610525576aa7e181a97f86dfd006e
Ahmad Afiq <afiqbokhari@hotmail.com>
2026-08-07 09:13:13 +0800

    fix: passed_to dropdown defaults to empty in create/edit

--- resources/views/front-desk/edit.blade.php
+++ resources/views/front-desk/edit.blade.php
@@ -145,6 +145,7 @@
                     @else
                         <select name="passed_to" id="passed_to"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <option value="">-- Select User --</option>
                             @foreach($legalUsers as $user)
                                 <option value="{{ $user->id }}" {{ old('passed_to', $frontDeskItem->passed_to) == $user->id ? 'selected' : '' }}>
                                     {{ $user->name }}
